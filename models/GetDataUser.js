4384f742a1230ae4c9451029ff210db0edb31806
Anukit Sornboonta <anukit084@hotmail.com>
2021-11-11 00:52:18 +0700

    update role

--- models/GetDataUser.js
+++ models/GetDataUser.js
@@ -3,7 +3,7 @@ const db = require("../dbconnection");
 var GetDataUser = {
   getdataUser: function (user_id, callback) {
     return db.query(
-      `SELECT email, firstname, lastname, image_profile FROM db_project."users" WHERE id = '${user_id}'`,
+      `SELECT email, firstname, lastname, image_profile, role FROM db_project."users" WHERE id = '${user_id}'`,
       callback
     );
   },
